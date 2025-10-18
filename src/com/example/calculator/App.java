417fa919ba8a041c3dbdede4c2c0bfc3f398f581
Jeongha Im <yimjh3205@gmail.com>
2025-10-18 19:50:34 +0900

    merge: feature/lv2-calculator-class into develop

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        // [6] Calculator 인스턴스 생성
+        Calculator cal = new Calculator();
+
         Scanner sc = new Scanner(System.in);
 
         // [4] "exit"하기 전까지 반복하기
@@ -17,41 +20,47 @@ public class App {
 
             // [2] 사칙연산 기호를 입력받기
             System.out.print("사칙연산 기호를 입력하세요(+, -, *, /) : ");
-            char symbol = sc.next().charAt(0);
+            char operator = sc.next().charAt(0);
 
 
-            // [3] 연산 후 결과값 출력하기
-            int result = 0;
-            boolean calculated = true;
+            // [3] 연산 후 결과값 출력하기 ---> // [7] Calculator 클래스로 연산하기
+            int result = cal.Calculate(num1, num2, operator);
 
-            switch (symbol) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        calculated = false;
-                    } else {
-                        result = num1 / num2;
-                    }
-                    break;
-                default:
-                    System.out.println("오류 : 유효하지 않은 연산 기호입니다.");
-                    calculated = false;
-                    break;
-            }
-
-            if (calculated) {
+            // [8] 결과 반환하기
+            if (result !=0) {
                 System.out.println("결과: " + result);
             }
 
+//            int result = 0;
+//            boolean calculated = true;
+//
+//            switch (operator) {
+//                case '+':
+//                    result = num1 + num2;
+//                    break;
+//                case '-':
+//                    result = num1 - num2;
+//                    break;
+//                case '*':
+//                    result = num1 * num2;
+//                    break;
+//                case '/':
+//                    if (num2 == 0) {
+//                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+//                        calculated = false;
+//                    } else {
+//                        result = num1 / num2;
+//                    }
+//                    break;
+//                default:
+//                    System.out.println("오류 : 유효하지 않은 연산 기호입니다.");
+//                    calculated = false;
+//                    break;
+//            }
+//
+//            if (calculated) {
+//                System.out.println("결과: " + result);
+//            }
 
             // [5] 계산 후, 종료 여부 묻기
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
