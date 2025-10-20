fddada371ca4146a3be1976cb7effbf3e2a96628
Jeongha Im <yimjh3205@gmail.com>
2025-10-20 16:36:17 +0900

    0으로 나눌 때 error 발생시키고 try-catch로 처리하기

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -1,4 +1,5 @@
 package com.example.calculator;
+import java.nio.channels.ScatteringByteChannel;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,79 +12,81 @@ public class App {
 
         // [7] "exit"하기 전까지 반복하기
         while(true) {
-
-            // [1] 양의 정수(0 포함)를 입력받기
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
-
-
-            // [2] 사칙연산 기호를 입력받기
-            System.out.print("사칙연산 기호를 입력하세요(+, -, *, /) : ");
-            char operator = sc.next().charAt(0);
+            try {
+                // [1] 양의 정수(0 포함)를 입력받기
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = sc.nextInt();
 
 
-            // [3] Calculator 클래스로 연산하기
-            int result = cal.Calculate(num1, num2, operator);
+                // [2] 사칙연산 기호를 입력받기
+                System.out.print("사칙연산 기호를 입력하세요(+, -, *, /) : ");
+                char operator = sc.next().charAt(0);
 
 
-            // [4] 결과 반환하기
-            if (result !=0) {
-                System.out.println("결과: " + result);
-            }
+                // [3] Calculator 클래스로 연산하기
+                int result = cal.Calculate(num1, num2, operator);
 
 
-            // [5] 결과 리스트 반환하기
-            // 게터 활용
-            ArrayList<Integer> arrResults = cal.getResults();
-            System.out.println("::: 결과 리스트 = " + arrResults);
-
-
-            // [6] 계산 후, 종료 여부 묻기
-            System.out.println("더 계산하시겠습니까? (exit : 입력 시 종료 / modify : 입력 시 수정 / delete : 입력 시 첫번째 값 삭제" +
-                    ")");
-            String answer = sc.next();
-
-
-            // [8] 세터를 활용한 수정 기능
-            if ("exit".equals(answer)) {
-                System.out.println("계산을 종료합니다.");
-                break;
-            } else if ("modify".equals(answer)) {
-                // 세터 활용
-                System.out.print("::: 수정할 인덱스를 입력하세요 : ");
-                int index = sc.nextInt();
-                System.out.print("::: 수정할 값을 입력하세요 : ");
-                int newValue = sc.nextInt();
-
-                // 수정 기능 호출
-                cal.setResults(index, newValue);
-                ArrayList<Integer> modifyResult = cal.getResults();
-                System.out.println("::: 수정 된 결과 리스트 = " + modifyResult);
-
-                // 수정 후 사용자에게 다음 행동을 다시 묻기
-                System.out.println("더 계산하시겠습니까? (exit : 입력 시 종료)");
-                String afterModify = sc.next();
-                if ("exit".equals(afterModify)) {
-                    break;
+                // [4] 결과 반환하기
+                if (result != 0) {
+                    System.out.println("결과: " + result);
                 }
-            }
 
-            // [9] 첫번째 인덱스 삭제 기능
-            if ("delete".equals(answer)) {
-                cal.removeResult();
 
-                // 삭제 후 결과 리스트 확인
-                ArrayList<Integer> modifyResult = cal.getResults();
-                System.out.println("::: 삭제 된 결과 리스트 = " + modifyResult);
+                // [5] 결과 리스트 반환하기
+                // 게터 활용
+                ArrayList<Integer> arrResults = cal.getResults();
+                System.out.println("::: 결과 리스트 = " + arrResults);
+
+
+                // [6] 계산 후, 종료 여부 묻기
+                System.out.println("더 계산하시겠습니까? (exit : 입력 시 종료 / modify : 입력 시 수정 / delete : 입력 시 첫번째 값 삭제" +
+                        ")");
+                String answer = sc.next();
+
+
+                // [8] 세터를 활용한 수정 기능
+                if ("exit".equals(answer)) {
+                    System.out.println("계산을 종료합니다.");
+                    break;
+                } else if ("modify".equals(answer)) {
+                    // 세터 활용
+                    System.out.print("::: 수정할 인덱스를 입력하세요 : ");
+                    int index = sc.nextInt();
+                    System.out.print("::: 수정할 값을 입력하세요 : ");
+                    int newValue = sc.nextInt();
+
+                    // 수정 기능 호출
+                    cal.setResults(index, newValue);
+                    ArrayList<Integer> modifyResult = cal.getResults();
+                    System.out.println("::: 수정 된 결과 리스트 = " + modifyResult);
+
+                    // 수정 후 사용자에게 다음 행동을 다시 묻기
+                    System.out.println("더 계산하시겠습니까? (exit : 입력 시 종료)");
+                    String afterModify = sc.next();
+                    if ("exit".equals(afterModify)) {
+                        break;
+                    }
+                } // [9] 첫번째 인덱스 삭제 기능
+                else if ("delete".equals(answer)) {
+                    cal.removeResult();
+
+                    // 삭제 후 결과 리스트 확인
+                    ArrayList<Integer> modifyResult = cal.getResults();
+                    System.out.println("::: 삭제 된 결과 리스트 = " + modifyResult);
 
                 // 삭제 후 사용자에게 다음 행동을 다시 묻기
-                System.out.println("더 계산하시겠습니까? (exit : 입력 시 종료)");
-                String afterModify = sc.next();
-                if ("exit".equals(afterModify)) {
-                    break;
+                    System.out.println("더 계산하시겠습니까? (exit : 입력 시 종료)");
+                    String afterModify = sc.next();
+                    if ("exit".equals(afterModify))
+                        break;
+
                 }
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("🚨 Error : " + e.getMessage());
             }
         }
     }
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,20 +1,12 @@
-/* 사칙연산을 수행 후, 결과값 반환 메서드 구현 & 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
-    - 사칙연산을 수행한 후, 결과값을 반환하는 메서드 구현
-    - 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
-       1) 양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(➕,➖,✖️,➗) 기능을 수행한 후
-       2) 결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성합니다.
-*/
-
 package com.example.calculator;
 import java.util.*;
 
 public class Calculator {
     // [1] 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
-    private ArrayList<Integer> results = new ArrayList<Integer>();
+    private ArrayList<Integer> results = new ArrayList<>();
 
 
     // [2] 사칙연산을 수행한 후, 결과값을 반환하는 메서드 구현
-    // 생성자
     public int Calculate(int num1, int num2, char operator) {
         int result = 0;
 
@@ -30,8 +22,7 @@ public class Calculator {
                 break;
             case '/':
                 if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    break;
+                    throw new IllegalArgumentException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                 } else {
                     result = num1 / num2;
                 }
